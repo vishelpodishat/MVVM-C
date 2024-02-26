93e7e77c9033cff39ce52d8c2f40ab1150a13cd9
vishelpodishat <alishsaideshov@gmail.com>
2024-02-27 01:07:30 +0600

    Controllers init

--- MVVM+Coordinator/SecondVC.swift
+++ MVVM+Coordinator/SecondVC.swift
@@ -1,64 +1,64 @@
 //
-//  ViewController.swift
+//  SecondVC.swift
 //  MVVM+Coordinator
 //
-//  Created by Alisher Saideshov on 26.02.2024.
+//  Created by Alisher Saideshov on 27.02.2024.
 //
 
 import UIKit
 
-final class ViewController: UIViewController {
+final class SecondVC: UIViewController {
 
     weak var coordinator: AppCoodinator?
 
     // MARK: - UI
-    private lazy var toSecondVCButton: UIButton = {
+    private lazy var toFirstVCButton: UIButton = {
         let button = UIButton(type: .system)
         button.layer.cornerRadius = 8
         button.layer.masksToBounds = true
         button.setTitleColor(.white, for: .normal)
-        button.setTitle("Second", for: .normal)
+        button.setTitle("First", for: .normal)
+        button.addTarget(self, action: #selector(toFirstVC), for: .touchUpInside)
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.addTarget(self, action: #selector(toSecondVC), for: .touchUpInside)
         return button
     }()
-    
+
     private lazy var toThirdVCButton: UIButton = {
         let button = UIButton(type: .system)
         button.layer.cornerRadius = 8
         button.layer.masksToBounds = true
         button.setTitleColor(.white, for: .normal)
         button.setTitle("Third", for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
         button.addTarget(self, action: #selector(toThirdVC), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .red
-        view.addSubview(toSecondVCButton)
+        view.backgroundColor = .blue
+        view.addSubview(toFirstVCButton)
         view.addSubview(toThirdVCButton)
         setupConstraints()
     }
-
+    
     private func setupConstraints() {
         NSLayoutConstraint.activate([
-            toSecondVCButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            toSecondVCButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            toSecondVCButton.heightAnchor.constraint(equalToConstant: 50),
-            toSecondVCButton.widthAnchor.constraint(equalToConstant: 200),
+            toFirstVCButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            toFirstVCButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            toFirstVCButton.heightAnchor.constraint(equalToConstant: 50),
+            toFirstVCButton.widthAnchor.constraint(equalToConstant: 200),
 
             toThirdVCButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            toThirdVCButton.centerYAnchor.constraint(equalTo: toSecondVCButton.centerYAnchor, constant: 30),
+            toThirdVCButton.centerYAnchor.constraint(equalTo: toFirstVCButton.centerYAnchor, constant: 30),
             toThirdVCButton.heightAnchor.constraint(equalToConstant: 50),
             toThirdVCButton.widthAnchor.constraint(equalToConstant: 200),
         ])
     }
 
-    @objc func toSecondVC() {
-        coordinator?.openSecondVC()
+    @objc func toFirstVC() {
+        coordinator?.openFirstVC()
     }
 
     @objc func toThirdVC() {
